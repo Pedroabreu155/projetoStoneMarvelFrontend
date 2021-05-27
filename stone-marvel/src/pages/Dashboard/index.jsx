5abbb1056b39f427ee13b2c47884e5e33f87bbfe
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-05-27 18:08:14 -0300

    dashboard done

--- stone-marvel/src/pages/Dashboard/index.jsx
+++ stone-marvel/src/pages/Dashboard/index.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 
+import { Card, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
+import { Tittle, Text } from './styles'
+
+import './Dashboard.css'
 
 export default function Dashboard(){
   return(
     <>
       <Navbar/>
+      <Container className="dashboard-wrapper">
+        <Link to="/comics" className="card-link">
+          <Card className="dashboard-card" style={{ width: '20rem', height: '20rem' }}>
+            <div className="card-content">
+              <Tittle>COMICS</Tittle>
+              <Text>Procure e descubra os principais HQs da Marvel</Text>
+              <img className="cardImageComics" src="/comics.png" alt="heroes"/>
+            </div>
+          </Card>
+        </Link>
+        <Link to="/characters" className="card-link">
+          <Card className="dashboard-card" style={{ width: '20rem', height: '20rem' }}>
+            <div className="card-content">
+              <Tittle>PERSONAGENS</Tittle>
+              <Text>Veja os grandes heróis da Marvel e aprenda sobre eles</Text>
+              <img className="cardImageHeroes" src="/heroes.png" alt="heroes"/>
+            </div>
+          </Card>
+        </Link>
+      </Container>
     </>
   )
 }
---
+++ stone-marvel/src/pages/Dashboard/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Tittle = styled.h1`
+  color: black;
+  font-size: 1.5rem;
+  margin-top: 1rem;
+  font-weight: bold;
+`
+
+export const Text = styled.h1`
+  color: rgb(141, 21, 21);
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 1rem;
+  margin-top: 1rem;
+`
