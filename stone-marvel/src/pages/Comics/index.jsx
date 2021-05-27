9e263d0fa733049842ffc75952e4df52171fab00
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-05-27 18:37:29 -0300

    started to create Comics and Characters page

--- stone-marvel/src/pages/Comics/index.jsx
+++ stone-marvel/src/pages/Comics/index.jsx
@@ -1,12 +1,103 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 
+import { useHistory, Link } from 'react-router-dom'
+import { Table, Button, Modal } from 'react-bootstrap'
 import Navbar from '../../components/Navbar'
 
+export default function Characters(){
 
-export default function Comics(){
+  const [allProducts, setAllProducts] = useState([])
+  const [productModel, setProductModel] = useState()
+
+  const [showModal, setShowModal] = useState(false);
+
+  function handleCloseModal(){
+    setShowModal(false)
+  }
+  function handleShowModal(){
+    setShowModal(true)
+  }
+
+  async function loadOneProduct(id){
+    const response = ['1234']
+    
+    setProductModel(response)
+
+    handleShowModal()
+  }
+
+  useEffect(() => {
+    loadTableWithData()
+  }, [])
+
+  async function loadTableWithData(){
+    const response = []
+    // console.log(response)
+    setAllProducts(response)
+  }
+
+  const history = useHistory()
+
+  function goHome(){
+    history.push('/')
+  }
+
   return(
     <>
-    <Navbar/>
+      <Navbar/>
+      <div className="container manage-products">
+          <br/>
+          <h1>Lista de HQs</h1>
+          <br/>
+          <div className="page-header">
+            <Link to="/dashboard"><Button onClick={goHome} className="add-productBtn" variant="warning">Voltar para Dashboard</Button></Link>
+          </div>
+          <br/>
+          <Table className="text-center" striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>Marca</th>
+                <th>Produto</th>
+                <th>Preço</th>
+                <th>Armazenamento</th>
+                <th>Tecnologia 5G</th>
+                <th>Opções</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allProducts.map(product => (
+                <>
+                <tr key={product}>
+                  <td>{product}</td>
+                  <td>{product}</td>
+                  <td>R$ {product}</td>
+                  <td>{product} GB</td>
+                  <td>
+                    <Button onClick={() => loadOneProduct(product)} className="mr-2" size="sm" variant="info">Ver</Button>
+                    <Modal show={showModal} onHide={handleCloseModal}>
+                      <Modal.Header>
+                        <Modal.Title>{productModel}</Modal.Title>
+                      </Modal.Header>
+
+                      <Modal.Body>
+                        <img className="modal-image" src={productModel} alt="aaa"/>
+                        <h2>{productModel}</h2>
+                        <h2>R$ {productModel}</h2>
+                      </Modal.Body>
+
+                      <Modal.Footer>
+                        <Button variant="primary" onClick={handleCloseModal}>Ok</Button>
+                      </Modal.Footer>
+                    </Modal>
+                  </td>
+              </tr>
+              </>
+
+              ))}
+              
+            </tbody>
+          </Table>
+      </div>
     </>
   )
 }
--- stone-marvel/src/routes.jsx
+++ stone-marvel/src/routes.jsx
@@ -32,7 +32,7 @@ export default function Routes() {
           <Route path="/" exact component={Home}/>
           <Route path="/signup" exact component={SignUp}/>
           <Route path="/dashboard" exact component={Dashboard}/>
-          <PrivateRoute path="/characters" exact component={Characters}/>
+          <Route path="/characters" exact component={Characters}/>
           <Route path="/comics" exact component={Comics}/>
           <PrivateRoute path="/profile" exact component={Profile}/>
           <PrivateRoute path="/favorites" exact component={Favorites}/>
